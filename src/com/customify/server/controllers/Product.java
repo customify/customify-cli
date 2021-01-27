0b47a4fd53ebbc8b31356d68993db13285572524
sandberg-smith <sandbergjacques500@gmail.com>
2021-01-27 23:25:51 +0200

    created a blueprint of product controller

---
+++ src/com/customify/server/controllers/Product.java
@@ -0,0 +1,37 @@
+package customify.server.controllers;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class Product {
+    DataOutputStream output;
+
+    public void registerProduct(Socket socket)throws IOException {
+        output = new DataOutputStream(socket.getOutputStream());
+        output.writeUTF("Product was registered successfully");
+    }
+
+    public void updateProduct(Socket socket) throws IOException{
+        output = new DataOutputStream(socket.getOutputStream());
+        output.writeUTF("Product was updated successfully");
+    }
+
+    public void deleteProduct(Socket socket)throws IOException {
+        output = new DataOutputStream(socket.getOutputStream());
+        output.writeUTF("Product was deleted successfully");
+    }
+
+    public void getAllProducts(Socket socket) throws IOException {
+        output = new DataOutputStream(socket.getOutputStream());
+        output.writeUTF("Products list here...");
+    }
+
+    public void getProduct(Socket socket) throws IOException{
+        output = new DataOutputStream(socket.getOutputStream());
+        output.writeUTF("Single product");
+    }
+}
+
+
+
