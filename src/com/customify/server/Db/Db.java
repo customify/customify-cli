ed0fa71368fff4c488cff1519b184507c698f8a3
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-01-29 08:44:48 +0200

    Added online db

--- src/com/customify/server/Db/Db.java
+++ src/com/customify/server/Db/Db.java
@@ -13,13 +13,23 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Db {
+
+
+    // To login into the shell do
+
+    // mysql -h bsnlibok3ll8qs778xbx-mysql.services.clever-cloud.com -P 3306 -u uqq6c1ewt1hkbzwd -p bsnlibok3ll8qs778xbx
+    // jaW3mRUAAwzTAiOTVkZu
+
   private static  Connection connection = null;
   private static Statement statement = null;
-  private static final String dbUrl = new String("");
-  private static final String user = new String("");
-  private static final String password = new String("");
+  private static final String dbUrl = "jdbc:mysql://uqq6c1ewt1hkbzwd:jaW3mRUAAwzTAiOTVkZu@bsnlibok3ll8qs778xbx-mysql.services.clever-cloud.com:3306/bsnlibok3ll8qs778xbx";
+  private static final String user = "uqq6c1ewt1hkbzwd";
+  private static final String password = "jaW3mRUAAwzTAiOTVkZu";
 
 
+    /**
+     * for initializing the connections to the database
+     */
     public static void init() {
       System.out.println("Db is connecting...........");
 
@@ -37,10 +47,16 @@ public class Db {
   }
 
 
+  /*
+  * Getting connection
+  * */
     public static Connection getConnection() {
         return connection;
     }
 
+    /*
+    * Statement to perform query on
+    * */
     public static Statement getStatement() {
         return statement;
     }
