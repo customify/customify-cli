845417f725f14f5d9789631c69995b5ac6d98f78
Gisa Kaze Fredson <fredson-coder@gmail.com>
2021-02-04 14:18:13 +0200

    Pass socket

--- src/com/customify/server/routes/PointsRoutes.java
+++ src/com/customify/server/routes/PointsRoutes.java
@@ -11,7 +11,7 @@ public class PointsRoutes {
     PointsController pointsController;
     Request request;
 
-    public PointsRoutes(Socket socket, Request request) {
+    public PointsRoutes(Socket socket) {
         this.socket = socket;
         this.pointsController = new PointsController(this.socket,this.request);
     }
