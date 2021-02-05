33a0e15aa23c2c213103ca61a0548be8ccb64bda
sandberg-smith <sandbergjacques500@gmail.com>
2021-02-05 16:32:10 +0200

    finished view all products functionality

--- src/com/customify/server/routes/HandleRoutes.java
+++ src/com/customify/server/routes/HandleRoutes.java
@@ -29,6 +29,9 @@ public class HandleRoutes {
         if (this.key ==Keys.CREATE_PRODUCT){
             productRoute.registerProduct();
         }
+        if (this.key ==Keys.CREATE_PRODUCT){
+            productRoute.getProducts();
+        }
         authRoute.loginError();
     }
 }
