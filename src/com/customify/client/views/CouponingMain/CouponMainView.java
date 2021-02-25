e62763570be336f18327c041429f2f6a566462a3
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-02-25 21:36:27 +0200

    Setup coupon and Sales View done

--- src/com/customify/client/views/CouponingMain/CouponMainView.java
+++ src/com/customify/client/views/CouponingMain/CouponMainView.java
@@ -1,7 +1,7 @@
 // By Makuza  Mugabo Verite on Feb 08 2021
 // This handles all views related to coupons
 
-package com.customify.client.views.AwardMangment;
+package com.customify.client.views.CouponingMain;
 
 import com.customify.client.data_format.CheckCoupon;
 import com.customify.client.data_format.CouponFormat;
@@ -11,49 +11,62 @@ import com.customify.client.services.CouponService;
 import java.net.Socket;
 import java.util.Scanner;
 
-public class CouponView {
+public class CouponMainView {
     Socket socket;
+    boolean isLoggedIn;
 
-    public CouponView(Socket socket) {
+    public CouponMainView(Socket socket) {
         this.socket = socket;
+    }
+
+
+
+    public CouponMainView(Socket socket, boolean isLoggedIn) throws Exception {
+        this.socket = socket;
+        this.setLoggedIn(isLoggedIn);
+
+        this.init();
     }
 
     public void init() throws Exception {
 
         Scanner reader = new Scanner(System.in);
 
-        int option;
+        boolean isViewEnabled = true;
 
-        System.out.println("|------------------------------------|");
-        System.out.println("| COUPONS                            |");
-        System.out.println("|____________________________________|");
-        System.out.println("|1.  create a coupon                  |");
-        System.out.println("|2.  Redeem a coupon                  |");
-        System.out.println("|3.  Check a coupon                   |");
-        System.out.println("|4.  Get available coupons            |");
-        System.out.println("|00. Back                             |");
-        System.out.println("|____________________________________|");
-        System.out.println("Enter option here -> ");
+        do {
+            System.out.println("|-------------------------------------|");
+            System.out.println("|           COUPON MANAGEMENT         |");
+            System.out.println("|_____________________________________|");
+            System.out.println("|1.  create a coupon                  |");
+            System.out.println("|2.  Redeem a coupon                  |");
+            System.out.println("|3.  Check a coupon                   |");
+            System.out.println("|4.  Get available coupons            |");
+            System.out.println("|00. Back                             |");
+            System.out.println("|_____________________________________|");
+            System.out.println("Enter option here -> ");
 
-        option = reader.nextInt();
+            int option = reader.nextInt();
 
-        switch (option){
-            case 1:
-                this.createCoupon();
-                break;
-            case 2:
-                this.redeemCoupon();
-                break;
-            case 3:
-                this.checkCoupon();
-                break;
-            case 4:
-                this.getAllCoupons();
-                break;
-            default:
-                System.out.println("You entered incorrect option");
-        }
-
+            switch (option){
+                case 1:
+                    this.createCoupon();
+                    break;
+                case 2:
+                    this.redeemCoupon();
+                    break;
+                case 3:
+                    this.checkCoupon();
+                    break;
+                case 4:
+                    this.getAllCoupons();
+                    break;
+                case 00:
+                    isViewEnabled = false;
+                default:
+                    System.out.println("You entered incorrect option");
+            }
+        }while (isViewEnabled);
     }
 
     public void createCoupon() throws Exception {
@@ -121,5 +134,13 @@ public class CouponView {
         //TODO: add response from the server here
         System.out.println("Response from server here");
         this.init();
+    }
+
+    public boolean isLoggedIn() {
+        return isLoggedIn;
+    }
+
+    public void setLoggedIn(boolean loggedIn) {
+        isLoggedIn = loggedIn;
     }
 }
