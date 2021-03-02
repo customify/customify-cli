b411fe3540f7c6034bed9f1af1359edcb9a8b223
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-03-02 23:09:59 +0200

    Reading from the backend

--- src/com/customify/client/services/SalesService.java
+++ src/com/customify/client/services/SalesService.java
@@ -2,9 +2,14 @@ package com.customify.client.services;
 
 import com.customify.client.SendToServer;
 import com.customify.client.data_format.SalesFormat;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
 import java.net.Socket;
+import java.util.List;
 
 public class SalesService {
     private Socket socket;
@@ -19,12 +24,23 @@ public class SalesService {
 
     public void getAllSales(String json) throws IOException {
         System.out.println(json);
-        System.out.println("Getting all sales!");
         SendToServer sendToServer = new SendToServer(json,this.socket);
         if(sendToServer.send()){
-            System.out.println("Boom! Done");
+            this.handleResponse();
         }
     }
 
     public void create(SalesFormat salesFormat){}
+
+    private void handleResponse(){
+
+        try{
+            InputStream inputStream = this.socket.getInputStream();
+            ObjectInputStream  objectInputStream = new ObjectInputStream(inputStream);
+            //ObjectMapper objectMapper = new ObjectMapper();
+            //JsonNode jsonNode = objectMapper.readTree(objectInputStream);
+        }catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+    }
 }
