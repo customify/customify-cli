38f18211ca2f579718a631b045bd2313b4742614
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-03-02 23:01:22 +0200

    sending data to client fixed

--- src/com/customify/server/services/SalesService.java
+++ src/com/customify/server/services/SalesService.java
@@ -2,9 +2,15 @@
 
 package com.customify.server.services;
 
+import com.customify.server.CustomizedObjectOutputStream;
 import com.customify.server.Db.Db;
-import com.fasterxml.jackson.databind.ObjectWriter;
+import com.customify.server.response_data_format.sale.SaleDataFormat;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.net.Socket;
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -14,13 +20,17 @@ import java.sql.Statement;
 public class SalesService {
   private Socket socket;
   private Connection connection = Db.getConnection();
+  private SaleDataFormat saleDataFormat;
+  ObjectMapper objectMapper = new ObjectMapper();
+  ObjectOutputStream objectOutputStream;
+  OutputStream outputStream;
 
     public SalesService(Socket socket) {
         this.socket = socket;
     }
 
-    public void getAllSales(){
-
+    public void getAllSales() throws IOException {
+      String jsonData = "";
         try{
             String query = "SELECT * FROM Sale";
 
@@ -29,10 +39,16 @@ public class SalesService {
             ResultSet resultSet = statement.executeQuery(query);
 
             while (resultSet.next()){
-                System.out.println("Id: "+resultSet.getInt(1)+" customerId: "+resultSet.getString(2)+" quantity: "+resultSet.getString(3)+" totalPrice: "+resultSet.getString(4)+" employeeID: "+resultSet.getString(5)+" productID: "+resultSet.getString(6));
+                this.saleDataFormat = new SaleDataFormat(resultSet.getInt(1),resultSet.getString(2),resultSet.getString(3),resultSet.getString(4),resultSet.getString(5),resultSet.getString(6));
+                jsonData += this.objectMapper.writeValueAsString(saleDataFormat);
             }
-        }catch (SQLException e){
+        }catch (SQLException | JsonProcessingException e){
             e.printStackTrace();
+            System.out.println("Error while sending to the client");
+        }finally {
+            outputStream = socket.getOutputStream();
+            this.objectOutputStream = new CustomizedObjectOutputStream(this.outputStream);
+            objectOutputStream.writeObject(jsonData);
         }
     }
 }
