a24f1021ba4fcfa2589f2549da1eb9e56032b3e1
fistonfle <fistonfle04@gmail.com>
2021-03-05 08:34:26 +0200

    Blss and product report on client

--- src/com/customify/client/services/reports/ProductReportService.java
+++ src/com/customify/client/services/reports/ProductReportService.java
@@ -2,25 +2,23 @@ package com.customify.client.services.reports;
 
 import com.customify.client.SendToServer;
 
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.net.Socket;
 import java.util.List;
 
-public class BusinessReportService {
+public class ProductReportService {
     private Socket socket;
     private InputStream input;
     private ObjectInputStream objectInput;
     private String json_data;
     private List<String> response;
-    BusinessReportService(){}
-    public BusinessReportService(Socket socket) {
+    public ProductReportService(){}
+    public ProductReportService(Socket socket) {
         this.socket = socket;
     }
-
-    public void getBlssWeekly(String json) throws IOException, ClassNotFoundException {
+    public void getProductsWeekly(String json) throws IOException, ClassNotFoundException {
         SendToServer serverSend = new SendToServer(json, this.socket);
         if (serverSend.send()) {
 
@@ -28,7 +26,7 @@ public class BusinessReportService {
             System.out.println("Request failed...");
         }
     }
-    public void getBlssDaily(String json) throws IOException, ClassNotFoundException {
+    public void getProductsDaily(String json) throws IOException, ClassNotFoundException {
         SendToServer serverSend = new SendToServer(json, this.socket);
         if (serverSend.send()) {
 
