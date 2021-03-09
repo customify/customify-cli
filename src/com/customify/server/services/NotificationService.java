f752ddfa4ea93e34ac1fa2a43cba7b884d7ad3a3
Gisa Kaze Fredson <fredson-coder@gmail.com>
2021-03-09 20:47:58 +0200

    Save notified winner

--- src/com/customify/server/services/NotificationService.java
+++ src/com/customify/server/services/NotificationService.java
@@ -15,16 +15,15 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class NotificationService {
-    /**
+    /*
      * @author yassin hagenimana
      * this is SendNotification method receives parameters of mail from, mail to, subject of email and descriprion of message to customer
      * who won an award.
-     **/
+     */
     public void SendNotification(String mailFrom, String password, String mailTo, String subject, String msg) {
 
         //Get properties object
         Properties props = new Properties();
-
         props.put("mail.smtp.host", "smtp.gmail.com");
         props.put("mail.smtp.socketFactory.port", "465");
         props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
@@ -56,35 +55,30 @@ public class NotificationService {
 
     /* this is method saving notification in database before sending it to customer.*/
 
-    public void SaveNotifications(){
+    public void SaveNotifications(String customer_id,String title,String description){
         Properties prop = new Properties();
         String fileName = "config.properties";
         InputStream is = null;
-        try {
-            is = new FileInputStream(fileName);
-        } catch (FileNotFoundException ex) {
-            System.out.println(ex.getMessage());
-        }
-        try {
-            prop.load(is);
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
-        try{
-            String query = "INSERT INTO Awards_Notifications(customer_id,title,description,created_at) VALUES(?,?,?,NOW())";
+        try { is = new FileInputStream(fileName); }
+            catch (FileNotFoundException ex) {
+            System.out.println(ex.getMessage()); }
+            try { prop.load(is); }
+             catch (IOException ex) {
+                System.out.println(ex.getMessage()); }
+
+          try{ String query = "INSERT INTO Awards_Notifications(customer_id,title,description,created_at) VALUES(?,?,?,NOW())";
             Connection connection = Db.getConnection();
             PreparedStatement statement = connection.prepareStatement(query);
-            statement.setInt(1,1);
-            statement.setString(2, prop.getProperty("subject"));
-            statement.setString(3, prop.getProperty("msg"));
+            statement.setString(1,customer_id);
+            statement.setString(2,title);
+            statement.setString(3, description);
 
             if(statement.execute())
                 System.out.println("Insertion done");
-            Db.closeConnection();
-
-        }catch (SQLException e){
-            System.out.println(e.getMessage());
-        }
+                Db.closeConnection(); }
+                catch (SQLException e){
+               System.out.println(e.getMessage());
+              }
     }
 
 
@@ -102,8 +96,9 @@ public class NotificationService {
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
-        SaveNotifications();
-       SendNotification(prop.getProperty("mailFrom"), prop.getProperty("mailPassword"), prop.getProperty("mailTo"), prop.getProperty("subject"),
-              prop.getProperty("msg"));
+
+//        SaveNotifications("1", prop.getProperty("subject"), prop.getProperty("msg"));
+       // SendNotification(prop.getProperty("mailFrom"), prop.getProperty("mailPassword"), prop.getProperty("mailTo"), prop.getProperty("subject"),
+          //    prop.getProperty("msg"));
     }
 }
